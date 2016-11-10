38b952323717a3c711a4c7d173bc6d785a593b69
Doug Bunting <dougbu@microsoft.com>
2016-11-10 12:00:20 -0800

    Add explicit `[Required]` attribute because `[MinLength]` allows `null`

--- src/Localized.1.1-preview/Models/Model.cs
+++ src/Localized.1.1-preview/Models/Model.cs
@@ -7,6 +7,7 @@ namespace Localized._1._1_preview.Models
         [Required(ErrorMessage = "{0} is totally, completely required.")]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} must be longer than 4 (four) characters.")]
         [MinLength(4, ErrorMessage = "{0} must be longer than {1} characters.")]
         public string Name { get; set; }
     }
